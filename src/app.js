3d93f6711962d16967129b5a49db5817f283c5c2
Manas1952 <purohitmanas36@gmail.com>
2022-01-20 17:08:59 +0530

    Add private routes

--- src/app.js
+++ src/app.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'  //We need to pass store to every component through props, which would be tedious, so it is going to pass 'store' to all of our components
-import AppRouter from './routers/AppRouter'
+import AppRouter, { history } from './routers/AppRouter'
 import configureStore from './store/configureStore'
 import { startSetExpenses } from './actions/expenses'
-import { setTextFilter } from './actions/filters'
+import { login, logout } from './actions/auth'
 import getVisibleEpense from './selectors/expenses'
 import 'normalize.css/normalize.css'
 import './styles/style.scss'
 import 'react-dates/lib/css/_datepicker.css'
-import './firebase/firebase'
+import { firebase } from './firebase/firebase'
 
 const store = configureStore()
 
@@ -18,9 +18,29 @@ const jsx = (
     <AppRouter />
   </Provider>
 )
+let hasRendered = false
+const renderApp = () => {
+  if (!hasRendered) {
+    ReactDOM.render(jsx, document.getElementById('app'))
+    hasRendered = true
+  }
+}
 
 ReactDOM.render(<p>Loading...</p>, document.getElementById('app'))
 
-store.dispatch(startSetExpenses()).then(() => {
-  ReactDOM.render(jsx, document.getElementById('app'))
+firebase.auth().onAuthStateChanged((user) => {
+  if (user) {
+    store.dispatch(login(user.uid))
+    store.dispatch(startSetExpenses()).then(() => {
+      renderApp()
+      if (history.location.pathname === '/') {
+        history.push('/dashboard')
+      }
+    })
+  }
+  else {
+    store.dispatch(logout())
+    renderApp()
+    history.push('/')
+  }
 })
